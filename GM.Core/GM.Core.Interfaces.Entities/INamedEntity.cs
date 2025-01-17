36bbcf20cc90bebed51445adccf49c62b9e768dd
Marat <milya010790@mail.ru>
2025-01-17 15:24:11 +0500

    Интерфейсы для сущностей

---
+++ GM.Core/GM.Core.Interfaces.Entities/INamedEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace GM.Core.Interfaces.Entities
+{
+    /// <summary>Именованная сущность</summary>
+    /// <typeparam name="TKey">Тип первичного ключа</typeparam>
+    public interface INamedEntity<out TKey> : IEntity<TKey>
+    {
+        /// <summary>Имя</summary>
+        string Name { get; }
+    }
+
+    /// <summary>Именованная сущность</summary>
+    public interface INamedEntity : INamedEntity<Guid>, IEntity { }
+}
