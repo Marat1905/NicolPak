652d19b6e9014a1d7c90133e853e68f7b26ebdfb
Marat <milya010790@mail.ru>
2025-02-17 12:56:26 +0500

    Добавлена модель для ПРС

---
+++ PrsService/Domain/PrsService.Domain.Entities/Roll.cs
@@ -0,0 +1,20 @@
+﻿// Ignore Spelling: Prs
+
+using GM.EFCore.Entities.Base;
+
+namespace PrsService.Domain.Entities
+{
+    public class Roll<TKey>: Entity<TKey>
+    {
+        /// <summary>Идентификатор переданный из ПЛК</summary>
+        public int IdIntRoll { get; set; }
+
+        /// <summary>Ширина рулона </summary>
+        public double Width { get; set; }
+
+        public required TKey ProductionId { get; set; }
+
+        public required Production Production { get; set; }
+    }
+    public class Roll : Roll<Guid> { }
+}
---
+++ PrsService/Domain/PrsService.Domain.Entities/TamburPrs.cs
@@ -0,0 +1,16 @@
+﻿// Ignore Spelling: Tambur Prs
+
+using GM.EFCore.Entities.Base;
+
+namespace PrsService.Domain.Entities
+{
+    /// <summary> Тамбур поставленный на ПРС</summary>
+    /// <typeparam name="TKey">Тип первичного ключа</typeparam>
+    public class TamburPrs<TKey>: TimedEntity<TKey>
+    {
+        public virtual ICollection<Production>? Productions { get; set; }
+    }
+
+    /// <summary><inheritdoc/> </summary>
+    public class TamburPrs : TamburPrs<Guid> { }
+}
