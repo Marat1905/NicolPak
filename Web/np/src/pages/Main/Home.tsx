3f0c5df29887829b24dfd8f62a3305b5080111cf
Marat <milya010790@mail.ru>
2025-04-15 16:20:18 +0500

    Забыл добавить

--- Web/np/src/pages/Main/Home.tsx
+++ Web/np/src/pages/Main/Home.tsx
@@ -1,4 +1,5 @@
 import AnalyticsMetrics from "../../components/analytics/AnalyticsMetrics";
+import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import PageMeta from "../../components/common/PageMeta";
 
 export default function Home() {
@@ -8,6 +9,8 @@ export default function Home() {
                 title="Домашняя - Главная"
                 description="Домашняя - Главная"
             />
+
+            <PageBreadcrumb pageTitle="Главная" />
            
             <div className="grid grid-cols-12 gap-2 md:gap-6">
                 <div className="col-span-12">
